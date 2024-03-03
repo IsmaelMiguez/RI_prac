8306239b1bae376b33b72d47aa571202732afc90
Godita <106154877+L0L4-chan@users.noreply.github.com>
2024-03-03 12:10:40 +0100

    Update WebIndexer.java
    convertida en runnable para poder ser ejecutable en hilos, implementada funcion run (esbozo)
    ver link en Discord

--- practica/src/main/java/es/udc/fi/ri/practica/WebIndexer.java
+++ practica/src/main/java/es/udc/fi/ri/practica/WebIndexer.java
@@ -26,7 +26,18 @@ import es.udc.fi.ri.practica.WebIndexer;
 //
 
 
-public class WebIndexer implements AutoCloseable {
+public class WebIndexer implements AutoCloseable, Runnable {
+	
+    static String INDEX_PATH = "index";
+    static String DOCS_PATH = null;
+    static boolean create = false;
+    static int numThreads = Runtime.getRuntime().availableProcessors();
+    static boolean infoThread= false;
+    static boolean infoIndex=false;
+    static boolean title=false;
+    static boolean body=false;
+    static boolean useAnalyzer=false;
+    
 	
 	public static void main(String[] args) throws Exception {
 	    String usage =
@@ -35,16 +46,7 @@ public class WebIndexer implements AutoCloseable {
 	            + "This indexes the documents in DOCS_PATH, creating a Lucene index"
 	            + "in INDEX_PATH that can be searched with SearchFiles\n";
 	   
-	    String INDEX_PATH = "index";
-	    String DOCS_PATH = null;
-	    boolean create = false;
-	    int numThreads = Runtime.getRuntime().availableProcessors();
-	    boolean infoThread= false;
-	    boolean infoIndex=false;
-	    boolean title=false;
-	    boolean body=false;
-	    boolean useAnalyzer=false;
-	    
+	
 	    if (args.length != 1) {
 			System.out.println("A folder is needed for the index");
 			  System.err.println("Usage: " + usage);
@@ -98,8 +100,11 @@ public class WebIndexer implements AutoCloseable {
 		      System.exit(1);
 		    }
 
+	   // * todo ver la division de datos 
+	    
+	    
+	    /*movido a la funcion run pendiente de revisar si este seria el punto de corte o se deben compartir los elementos inicializados, 
 	   
-	    //
 	    final Path docDir = Paths.get(DOCS_PATH);
 	    if (!Files.isReadable(docDir)) {
 	      System.out.println(
@@ -109,7 +114,8 @@ public class WebIndexer implements AutoCloseable {
 	      System.exit(1);
 	    }
 
-	    Date start = new Date();
+	  
+	    *  Date start = new Date();
 	    try {
 	      System.out.println("Indexing to directory '" + INDEX_PATH + "'...");
 
@@ -124,7 +130,7 @@ public class WebIndexer implements AutoCloseable {
 	      } else {
 	        // Add new documents to an existing index:
 	        iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
-	      }
+	      }*/
 
 	      // Optional: for better indexing performance, if you
 	      // are indexing many documents, increase the RAM
@@ -154,7 +160,7 @@ public class WebIndexer implements AutoCloseable {
 	        // writer.forceMerge(1);
 	      } finally {
 	        IOUtils.close(vectorDictInstance);
-	      }*/
+	      }
 
 	      Date end = new Date();
 	      try (IndexReader reader = DirectoryReader.open(dir)) {
@@ -173,7 +179,7 @@ public class WebIndexer implements AutoCloseable {
 	      }
 	    } catch (IOException e) {
 	      System.out.println(" caught a " + e.getClass() + "\n with message: " + e.getMessage());
-	    }
+	    }*/
 	  }
 	
 
@@ -182,9 +188,60 @@ public class WebIndexer implements AutoCloseable {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	
-	
 
+
+	@Override
+	public void run() {
+		// Parte del codigo que se ejecutara en el hilo
+		
+		final Path docDir = Paths.get(DOCS_PATH);
+	    if (!Files.isReadable(docDir)) {
+	      System.out.println(
+	          "Document directory '"
+	              + docDir.toAbsolutePath()
+	              + "' does not exist or is not readable, please check the path");
+	      System.exit(1);
+	    }
+		
+		
+		 Date start = new Date();
+		    try {
+		      System.out.println("Indexing to directory '" + INDEX_PATH + "'...");
+
+		      Directory dir = FSDirectory.open(Paths.get( INDEX_PATH));
+		      Analyzer analyzer = new StandardAnalyzer();
+		      IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+
+		      if (create) {
+		        // Create a new index in the directory, removing any
+		        // previously indexed documents:
+		        iwc.setOpenMode(OpenMode.CREATE);
+		      } else {
+		        // Add new documents to an existing index:
+		        iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+		      }
+
+		      
+		      Date end = new Date();
+		      try (IndexReader reader = DirectoryReader.open(dir)) {
+		        System.out.println(
+		            "Indexed "
+		                + reader.numDocs()
+		                + " documents in "
+		                + (end.getTime() - start.getTime())
+		                + " ms");
+		        if (reader.numDocs() > 100
+		           // && vectorDictSize < 1_000_000
+		            && System.getProperty("smoketester") == null) {
+		          throw new RuntimeException(
+		              "Are you (ab)using the toy vector dictionary? See the package javadocs to understand why you got this exception.");
+		        }
+		      }
+		    } catch (IOException e) {
+		      System.out.println(" caught a " + e.getClass() + "\n with message: " + e.getMessage());
+		
+		    }
+		}
 }
+
 
