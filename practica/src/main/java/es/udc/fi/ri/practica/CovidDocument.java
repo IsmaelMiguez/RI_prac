ca1ad673af679a884dcc4a35261075440df64c04
Godita <106154877+L0L4-chan@users.noreply.github.com>
2024-04-15 16:59:50 +0200

    Update CovidDocument.java
    Change for working file

--- practica/src/main/java/es/udc/fi/ri/practica/CovidDocument.java
+++ practica/src/main/java/es/udc/fi/ri/practica/CovidDocument.java
@@ -1,10 +1,19 @@
 package es.udc.fi.ri.practica;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record CovidDocument( String id, String title, String text,String url,String pubmed_id) 
+public record CovidDocument( @JsonProperty("_id") String id, String title, String text, Metadata metadata) 
 {
+	 @JsonIgnoreProperties(ignoreUnknown = true)
+	  record Metadata(String url, String pubmed_id) {
 
+		public String url() {
+			return url;
+		}
+
+		public String pubmed_id() {
+			return pubmed_id;
+		}}
 }
+
